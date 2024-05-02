7dcf908f4878f5ac6ad0fbde6e30ad1656560567
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-03 00:10:46 +0300

    advisors

--- country.py
+++ country.py
@@ -4,6 +4,14 @@ class Country:
 
     def __init__(self):
         self.regions = []
+        self.civil_constr_bonus = 0
+        self.mil_constr_bonus = 0
 
     def add_region(self, region):
         self.regions.append(region)
+
+    def add_military_advisor(self):
+        self.mil_constr_bonus = 0.1
+
+    def add_civil_advisor(self):
+        self.civil_constr_bonus = 0.1
