e80dd95a29bf9b336fa7f91524a8f2f1d5238d7f
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-03 00:00:00 +0300

    Create region.py

---
+++ region.py
@@ -0,0 +1,32 @@
+
+
+class Region:
+
+    def __init__(self, name,
+                 max_factories, infrastructure,
+                 factories, military_factories):
+        self.name = name
+        self.factories_limit = max_factories
+        self.init_fact_limit = max_factories
+        self.infrastructure = infrastructure
+        self.factories = factories
+        self.military_factories = military_factories
+        self.oil = 0
+        self.aluminum = 0
+        self.rubber = 0
+        self.tungsten = 0
+        self.steel = 0
+        self.chromium = 0
+
+    def add_resources(self, oil, aluminum, rubber,
+                      tungsten, steel, chromium):
+        self.oil = oil
+        self.aluminum = aluminum
+        self.rubber = rubber
+        self.tungsten = tungsten
+        self.steel = steel
+        self.chromium = chromium
+
+    def recalculate_factories_limit(self, science_bonus):
+        self.factories_limit = (1+science_bonus)*self.init_fact_limit
+
