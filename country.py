1ce2e617f7714bc0da4b57e351fb5369bcbb8eae
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-03 11:17:05 +0300

    industry tech

--- country.py
+++ country.py
@@ -9,8 +9,14 @@ class Country:
 
     def __init__(self):
         self.regions = []
+        self.constr_bonus = 0
         self.civil_constr_bonus = 0
         self.mil_constr_bonus = 0
+        self.mil_output_bonus = 0
+        self.constr_tech = 0
+        self.industry_tech = 0
+        self.ind_and_constr_tech_limit = 5
+        self._distributed_industry = False
 
     def add_region(self, region):
         self.regions.append(region)
@@ -20,3 +26,30 @@ class Country:
 
     def add_civil_advisor(self):
         self.civil_constr_bonus = 0.1
+
+    @property
+    def distributed_industry(self):
+        return self._distributed_industry
+
+    @distributed_industry.setter
+    def distributed_industry(self, boolean):
+        if self.industry_tech != 0:
+            raise Exception("Нельзя сменить технологию")
+        self._distributed_industry = True
+
+    def upgrade_industry_tech(self):
+        if self.industry_tech >= 5:
+            raise Exception("Лимит технологии 5 уровень")
+        elif self._distributed_industry:
+            self.industry_tech += 1
+            self.mil_output_bonus += 0.1
+        else:
+            self.industry_tech += 1
+            self.mil_output_bonus += 0.15
+
+    def upgrade_construction_tech(self):
+        if self.constr_tech >= 5:
+            raise Exception("Лимит технологии 5 уровень")
+        else:
+            self.constr_tech += 1
+            self.constr_bonus += 0.1
