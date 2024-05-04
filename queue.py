8aa966f3f727b6bd7ae8765fa2191fac3c072739
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-04 19:07:15 +0300

    Update queue.py

--- queue.py
+++ queue.py
@@ -4,6 +4,6 @@ class Order:
 
     def __init__(self, region_name, building_type, quantity):
         self.target_region = region_name
-        self.target_region_id = 0
+        self.target_region_id = None
         self.building_type = building_type
         self.quantity = quantity
