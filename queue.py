663b9817cc244cf9bee4e7f0c0558d0a521b800c
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-04 18:54:01 +0300

    order

--- queue.py
+++ queue.py
@@ -2,5 +2,8 @@
 
 class Order:
 
-    def __init__(self):
-        pass
+    def __init__(self, region_name, building_type, quantity):
+        self.target_region = region_name
+        self.target_region_id = 0
+        self.building_type = building_type
+        self.quantity = quantity
