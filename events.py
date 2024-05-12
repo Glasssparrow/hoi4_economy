f8afd57390686040299343c9d4afa0a27e37693a
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-12 20:51:54 +0300

    Update events.py

--- events.py
+++ events.py
@@ -1,9 +1,13 @@
-
+from queue import Order
 
 class Event:
 
-    def __init__(self):
-        self.date = 0
+    def __init__(self, date,
+                 order: Order = None,
+                 policies: dict = None):
+        self.date = date
+        self.order = order
+        self.policies = policies
 
     def activate(self, owner):
         pass
