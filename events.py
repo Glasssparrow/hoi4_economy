d8082f2be1675ea373c64422ac10d72c15948727
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-14 22:26:21 +0300

    Update events.py

--- events.py
+++ events.py
@@ -15,5 +15,18 @@ class Event:
 
     def _activate_policies(self, owner):
         for k, v in self.policies.values():
-            if k not in []:
+            if k not in [
+                "military_advisor",
+                "civil_advisor",
+                "distributed_industry",
+                "industry_tech",
+                "construction_tech",
+                "move_conscription",
+                "move_trade",
+                "move_economy",
+                "add_consumer_goods",
+                "get_civil_constr_bonus",
+                "get_mil_constr_bonus",
+                "get_inf_constr_bonus",
+            ]:
                 raise Exception(f"{k} не корректное событие!")
