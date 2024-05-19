e9d89b9d345926916edaa14d423fc94d97533979
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-19 23:23:23 +0300

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -1,5 +1,7 @@
 from region import Region
+from queue import Order
 from country import Country
+from constants import *
 
 
 class Test1:
@@ -31,9 +33,30 @@ class Test1:
             "Yarkand", 1,
             1, 1, 0
         ))
+        self.country.add_order(Order(
+            region_name="Dabancheng",
+            building_type=CIVIL_BUILDING,
+            quantity=1,
+        ))
         self.constr365 = 6132
 
     def check(self):
         for x in range(365):
             self.country.calculate_day()
-        return True
+        # print(self.country.regions[0].civil_constr_progress)
+        # print(f"Доступно фабрик {self.country.factories_available}")
+        # print(self.country.consumer_goods)
+        # print(self.country.factories_for_consumers)
+        if (
+                self.constr365 - 50 <
+                self.country.regions[0].civil_constr_progress
+                < self.constr365 + 50
+        ):
+            return True
+        else:
+            print(
+                f"Целевой результат {self.constr365}.\n"
+                f"Полученный результат "
+                f"{self.country.regions[0].civil_constr_progress}."
+            )
+            return False
