401973f2032d79e568edb1297b11e82553d45f34
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-19 23:26:54 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
 from tests.test1 import Test1
 
 
+# Печатать ли результаты
+print_text = True
+
 something_wrong = False
 for test in [Test1]:
     x = Test1()
-    if not x.check():
+    if not x.check(print_text):
         print(f"Тест {x.name} не пройден.")
         something_wrong = True
 
