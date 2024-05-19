c01165470fac87630b1559ef2b0700bc1cf41e87
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-19 20:57:21 +0300

    some test1 preparation
    test still not finished

--- tests/test1.py
+++ tests/test1.py
@@ -6,8 +6,34 @@ class Test1:
     name = "Name"
 
     def __init__(self):
-        pass
+        self.country = Country("Sinkiang")
+        self.country.add_region(Region(
+            "Dabancheng", 2,
+            2, 0, 1
+        ))
+        self.country.add_region(Region(
+            "Dzungaria", 1,
+            2, 1, 0
+        ))
+        self.country.add_region(Region(
+            "Kunlun Shan", 0,
+            1, 0, 0
+        ))
+        self.country.add_region(Region(
+            "Urumqi", 2,
+            2, 2, 0
+        ))
+        self.country.add_region(Region(
+            "Taklamakan", 0,
+            1, 0, 0
+        ))
+        self.country.add_region(Region(
+            "Yarkand", 1,
+            1, 1, 0
+        ))
+        self.constr365 = 6132
 
-    @staticmethod
-    def check():
+    def check(self):
+        for x in range(365):
+            self.country.calculate_day()
         return True
