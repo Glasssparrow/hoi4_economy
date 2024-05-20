3e013de93b74a48ac36f71c494d3e49e061150bd
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-20 20:08:41 +0300

    Update events.py

--- events.py
+++ events.py
@@ -4,36 +4,27 @@ from constants import *
 
 class Event:
 
-    def __init__(self, date,
-                 order: Order = None,
-                 policies: dict = None):
+    def __init__(self, date, order_text):
         self.date = date
-        self.order = order
-        self.policies = policies
+        self.order = order_text
 
     def activate(self, owner):
-        if self.order is None:
-            return
-        else:
-            owner.add_order(self.order)
-        self._activate_policies(owner)
+        order = self._order_from_text()
+        if order[0] not in [
+            ADD_MILITARY_ADVISOR_COMMAND,
+            ADD_CIVIL_ADVISOR_COMMAND,
+            CHANGE_INDUSTRY_TYPE_COMMAND,
+            UPGRADE_INDUSTRY_TECH_COMMAND,
+            UPGRADE_CONSTRUCTION_TECH_COMMAND,
+            CHANGE_CONSCRIPTION_COMMAND,
+            CHANGE_TRADE_COMMAND,
+            CHANGE_ECONOMY_COMMAND,
+            GET_CIVIL_CONSTR_BONUS_COMMAND,
+            GET_MIL_CONSTR_BONUS_COMMAND,
+            GET_INF_CONSTR_BONUS_COMMAND,
+            ADD_CONSUMER_GOODS_COMMAND,
+        ]:
+            raise Exception(f"{order} не корректное событие!")
 
-    def _activate_policies(self, owner):
-        if self.policies is None:
-            return
-        for k, v in self.policies.values():
-            if k not in [
-                ADD_MILITARY_ADVISOR_COMMAND,
-                ADD_CIVIL_ADVISOR_COMMAND,
-                CHANGE_INDUSTRY_TYPE_COMMAND,
-                UPGRADE_INDUSTRY_TECH_COMMAND,
-                UPGRADE_CONSTRUCTION_TECH_COMMAND,
-                CHANGE_CONSCRIPTION_COMMAND,
-                CHANGE_TRADE_COMMAND,
-                CHANGE_ECONOMY_COMMAND,
-                ADD_CONSUMER_GOODS_COMMAND,
-                GET_CIVIL_CONSTR_BONUS_COMMAND,
-                GET_MIL_CONSTR_BONUS_COMMAND,
-                GET_INF_CONSTR_BONUS_COMMAND,
-            ]:
-                raise Exception(f"{k} не корректное событие!")
+    def _order_from_text(self):
+        return [self.order, self.order]
