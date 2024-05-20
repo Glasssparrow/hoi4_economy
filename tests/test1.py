248ef0aecadeb79f37e373512da5840c6f90370f
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-20 11:40:03 +0300

    Update test1.py

--- tests/test1.py
+++ tests/test1.py
@@ -4,40 +4,43 @@ from country import Country
 from constants import *
 
 
+country = Country("Sinkiang")
+country.add_region(Region(
+    "Dabancheng", 2,
+    2, 0, 1
+))
+country.add_region(Region(
+    "Dzungaria", 1,
+    2, 1, 0
+))
+country.add_region(Region(
+    "Kunlun Shan", 0,
+    1, 0, 0
+))
+country.add_region(Region(
+    "Urumqi", 2,
+    2, 2, 0
+))
+country.add_region(Region(
+    "Taklamakan", 0,
+    1, 0, 0
+))
+country.add_region(Region(
+    "Yarkand", 1,
+    1, 1, 0
+))
+country.add_order(Order(
+    region_name="Dabancheng",
+    building_type=CIVIL_BUILDING,
+    quantity=1,
+))
+
+
 class Test1:
     name = "Упрощенный Синцзянский"
 
     def __init__(self):
-        self.country = Country("Sinkiang")
-        self.country.add_region(Region(
-            "Dabancheng", 2,
-            2, 0, 1
-        ))
-        self.country.add_region(Region(
-            "Dzungaria", 1,
-            2, 1, 0
-        ))
-        self.country.add_region(Region(
-            "Kunlun Shan", 0,
-            1, 0, 0
-        ))
-        self.country.add_region(Region(
-            "Urumqi", 2,
-            2, 2, 0
-        ))
-        self.country.add_region(Region(
-            "Taklamakan", 0,
-            1, 0, 0
-        ))
-        self.country.add_region(Region(
-            "Yarkand", 1,
-            1, 1, 0
-        ))
-        self.country.add_order(Order(
-            region_name="Dabancheng",
-            building_type=CIVIL_BUILDING,
-            quantity=1,
-        ))
+        self.country = country
         self.constr365 = 6132
 
     def check(self, text=False):
