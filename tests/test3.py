6d3dd26a573e7b1aa2d7bf6fa9c4cc2c2d57aef4
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-20 18:58:59 +0300

    Update test3.py

--- tests/test3.py
+++ tests/test3.py
@@ -12,12 +12,16 @@ class Test3:
         self.days = {
             0: (0, 0, 0,),  # старт
             365: (2092, 5455, 0),  # 1 января 1937
+            366: (2241, 5525, 0),  # 2 января 1937
+            396: (6696, 7604, 0),  # 1 февраля 1937
             730: (0, 0, 4517)  # 1 января 1938
         }
         # день: (фабрики, военные_заводы, верфи)
         self.industry = {
             0: (29, 8, 11),  # старт
             365: (32, 8, 11),  # 1 января 1937
+            366: (32, 8, 11),  # 2 января 1937
+            396: (32, 8, 11),  # 1 февраля 1937
             730: (39, 8, 11),  # 1 января 1938
         }
 
@@ -26,6 +30,10 @@ class Test3:
         regions = self.country.regions
         no_problems = True
         for day in range(731):
+            if day == 365:
+                self.country.add_civil_advisor()
+                self.country.move_economy(+1)
+                self.country.move_economy(+1)
             if day in self.days.keys():
                 # print("День #", day)
                 # print(self.days[day][0],
@@ -38,6 +46,7 @@ class Test3:
                 #       )
 
                 no_problem_in_the_day = True
+                # Проверяем прогресс строительства в 3 выбранных регионах.
                 for x in range(3):
                     if (
                         floor(regions[region_ids[x]].civil_constr_progress)
@@ -46,6 +55,16 @@ class Test3:
                     ):
                         no_problems = False
                         no_problem_in_the_day = False
+                for index, quantity_of_buildings in {
+                    0: self.country.factories,
+                    1: self.country.mil_factories,
+                    2: self.country.shipyards,
+                }.items():
+                    if quantity_of_buildings != self.industry[day][index]:
+                        no_problems = False
+                        no_problem_in_the_day = False
+
+                # Выводим на экран если что-то не сходится
                 if not no_problem_in_the_day:
                     for_print = []
                     for i in region_ids:
@@ -59,6 +78,11 @@ class Test3:
                             f"{self.days[day][1]}, "
                             f"{self.days[day][2]}]/"
                             f"{for_print}. "
+                            f"Количество зданий ожидали/получили "
+                            f"{self.industry[day]}/"
+                            f"({self.country.factories}, "
+                            f"{self.country.mil_factories}, "
+                            f"{self.country.shipyards})."
                         )
             self.country.calculate_day()
         return no_problems
