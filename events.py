3f7efa2eb6df95b096666f039ce2f4c812bdef14
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-20 21:20:20 +0300

    Update events.py

--- events.py
+++ events.py
@@ -7,26 +7,38 @@ class Event:
         self.order = self._order_from_text(order_text)
 
     def activate(self, owner):
-        if self.order[0] not in [
-            BUILD_CIVIL_FACTORY,
-            BUILD_MIL_FACTORY,
-            BUILD_INFRASTRUCTURE,
-
-            ADD_MILITARY_ADVISOR_COMMAND,
-            ADD_CIVIL_ADVISOR_COMMAND,
-            CHANGE_INDUSTRY_TYPE_COMMAND,
-            UPGRADE_INDUSTRY_TECH_COMMAND,
-            UPGRADE_CONSTRUCTION_TECH_COMMAND,
-            CHANGE_CONSCRIPTION_COMMAND,
-            CHANGE_TRADE_COMMAND,
-            CHANGE_ECONOMY_COMMAND,
-            GET_CIVIL_CONSTR_BONUS_COMMAND,
-            GET_MIL_CONSTR_BONUS_COMMAND,
-            GET_INF_CONSTR_BONUS_COMMAND,
-            ADD_CONSUMER_GOODS_COMMAND,
-        ]:
+        if self.order[0] == BUILD_CIVIL_FACTORY:
+            pass
+        elif self.order[0] == BUILD_MIL_FACTORY:
+            pass
+        elif self.order[0] == BUILD_INFRASTRUCTURE:
+            pass
+        elif self.order[0] == ADD_MILITARY_ADVISOR_COMMAND:
+            pass
+        elif self.order[0] == ADD_CIVIL_ADVISOR_COMMAND:
+            pass
+        elif self.order[0] == CHANGE_INDUSTRY_TYPE_COMMAND:
+            pass
+        elif self.order[0] == UPGRADE_INDUSTRY_TECH_COMMAND:
+            pass
+        elif self.order[0] == UPGRADE_CONSTRUCTION_TECH_COMMAND:
+            pass
+        elif self.order[0] == CHANGE_CONSCRIPTION_COMMAND:
+            pass
+        elif self.order[0] == CHANGE_TRADE_COMMAND:
+            pass
+        elif self.order[0] == CHANGE_ECONOMY_COMMAND:
+            pass
+        elif self.order[0] == GET_CIVIL_CONSTR_BONUS_COMMAND:
+            pass
+        elif self.order[0] == GET_MIL_CONSTR_BONUS_COMMAND:
+            pass
+        elif self.order[0] == GET_INF_CONSTR_BONUS_COMMAND:
+            pass
+        elif self.order[0] == ADD_CONSUMER_GOODS_COMMAND:
+            pass
+        else:
             raise Exception(f"{self.order} не корректное событие!")
-        return self.order
 
     @staticmethod
     def _order_from_text(order_text):
