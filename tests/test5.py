460d40c3be64c27284eed7b6fa6eefd94b8ff1a7
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-22 01:42:41 +0300

    test5 preparations

--- tests/test5.py
+++ tests/test5.py
@@ -10,6 +10,23 @@ class Test5:
     def __init__(self):
         data = get_data()
         self.country = get_country(data=data, name_or_tag="France")
+        self.factories = 0
+        self.mil_factories = 0
+        self.shipyards = 0
 
     def check(self, text=False):
-        return False
+        if (self.factories == self.country.factories and
+            self.mil_factories == self.country.mil_factories and
+            self.shipyards == self.country.shipyards
+        ):
+            return True
+        else:
+            print(
+                f"Целевые показатели:    фабрики/в.заводы/верфи "
+                f"{self.factories}/{self.mil_factories}/{self.shipyards}.\n"
+                f"Полученные результаты: фабрики/в.заводы/верфи "
+                f"{self.country.factories}/"
+                f"{self.country.mil_factories}/"
+                f"{self.country.shipyards}."
+            )
+            return False
