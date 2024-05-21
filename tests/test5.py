c59202547a8f6a50f474cb88699f593e493804a6
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-22 01:48:35 +0300

    Update test5.py

--- tests/test5.py
+++ tests/test5.py
@@ -1,7 +1,4 @@
-from .common import floor
 from create_a_country import get_data, get_country
-from constants import *
-from events import Event
 
 
 class Test5:
@@ -15,18 +12,20 @@ class Test5:
         self.shipyards = 11
 
     def check(self, text=False):
-        if (self.factories == self.country.factories and
+        if (
+            self.factories == self.country.factories and
             self.mil_factories == self.country.mil_factories and
             self.shipyards == self.country.shipyards
         ):
             return True
         else:
-            print(
-                f"Целевые показатели:    фабрики/в.заводы/верфи "
-                f"{self.factories}/{self.mil_factories}/{self.shipyards}.\n"
-                f"Полученные результаты: фабрики/в.заводы/верфи "
-                f"{self.country.factories}/"
-                f"{self.country.mil_factories}/"
-                f"{self.country.shipyards}."
-            )
-            return False
+            if text:
+                print(
+                    f"Целевые показатели:    фабрики/в.заводы/верфи "
+                    f"{self.factories}/{self.mil_factories}/{self.shipyards}.\n"
+                    f"Полученные результаты: фабрики/в.заводы/верфи "
+                    f"{self.country.factories}/"
+                    f"{self.country.mil_factories}/"
+                    f"{self.country.shipyards}."
+                )
+                return False
