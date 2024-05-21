04bf242413e36e5884b4eb270ca5df66c0a1df08
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-22 01:28:34 +0300

    Update create_a_country.py

--- create_a_country.py
+++ create_a_country.py
@@ -1,5 +1,6 @@
 from json import load
 from country import Country
+from region import Region
 
 
 def get_data():
@@ -8,7 +9,8 @@ def get_data():
     return data
 
 
-def get_country(data, name_or_tag, by_tag=False):
+def get_country(data: dict, name_or_tag: str, by_tag=False):
+    name_or_tag = name_or_tag.lower()
     # Ищем название государства. Поднимаем ошибку если не нашли.
     name, tag, not_found = "name_not_found", "tag_not_found", True
     if by_tag:
@@ -29,3 +31,18 @@ def get_country(data, name_or_tag, by_tag=False):
         raise Exception("Country name/tag not found!")
 
     country = Country(name=name, tag=tag)
+    for number, region_data in data.items():
+        if country.tag == region_data["owner"]:
+            country.add_region(Region(
+                name=name,
+                max_factories=region_data["max_factories"],
+                infrastructure=region_data["infrastructure"],
+                factories=region_data["factories"],
+                military_factories=region_data["military_factories"],
+                shipyards=region_data["shipyards"],
+                fuel_silo=region_data["fuel_silo"],
+                synth_oil=region_data["synth_oil"],
+            ))
+    country.preparations()
+    return country
+
