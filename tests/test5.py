f4f2f659ed3bc20af0d879aac5caff3f0854c240
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-22 00:44:33 +0300

    Create test5.py

---
+++ tests/test5.py
@@ -0,0 +1,13 @@
+from .common import floor
+from constants import *
+from events import Event
+
+
+class Test4:
+    name = "читаем данные Франция"
+
+    def __init__(self):
+        pass
+
+    def check(self, text=False):
+        return False
