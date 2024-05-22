3da157fe581000ef0b39e66077162fbfb8b76025
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-22 13:10:28 +0300

    FRA test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ from tests.test4 import Test4
 from tests.test5 import Test5
 from tests.test6 import Test6
 from tests.test7 import Test7
+from tests.test8 import Test8
 
 
 # Печатать ли результаты
@@ -14,7 +15,7 @@ print_text = True
 # Не знаю что с этим делать, думаю пока оставлю.
 # Странно что описание из файлов игры не совпадает с тем что я вижу в игре.
 something_wrong = False
-for test in [Test1, Test2, Test3, Test4, Test5, Test6, Test7]:
+for test in [Test1, Test2, Test3, Test4, Test5, Test6, Test7, Test8]:
     x = test()
     if not x.check(print_text):
         print(f"Тест {x.name} не пройден.")
---
+++ tests/test8.py
@@ -0,0 +1,31 @@
+from create_a_country import get_data, get_country
+
+
+class Test8:
+    name = "читаем данные Франция"
+
+    def __init__(self):
+        data = get_data()
+        self.country = get_country(data=data, name_or_tag="FRA", by_tag=True)
+        self.factories = 33
+        self.mil_factories = 8
+        self.shipyards = 11
+
+    def check(self, text=False):
+        if (
+            self.factories == self.country.factories and
+            self.mil_factories == self.country.mil_factories and
+            self.shipyards == self.country.shipyards
+        ):
+            return True
+        else:
+            if text:
+                print(
+                    f"Целевые показатели:    фабрики/в.заводы/верфи "
+                    f"{self.factories}/{self.mil_factories}/{self.shipyards}.\n"
+                    f"Полученные результаты: фабрики/в.заводы/верфи "
+                    f"{self.country.factories}/"
+                    f"{self.country.mil_factories}/"
+                    f"{self.country.shipyards}."
+                )
+                return False
