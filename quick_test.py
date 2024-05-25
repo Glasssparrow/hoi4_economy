68363faef376ff8bf349a47a2542606c5741f141
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-25 14:54:46 +0300

    Update quick_test.py

--- quick_test.py
+++ quick_test.py
@@ -1,13 +1,19 @@
-from os import path, listdir
-from json import dump
+# comp100 = -0.083
+# comp95 = -0.079
+# comp70 = -0.058
+#
+# print(f"{comp70/70}:{comp70}/{-0.00083*70}",
+#       f"{comp95/95}:{comp95}/{-0.00083*95}",
+#       f"{comp100/100}:{comp100}/{-0.00083*100}")
 
-PATH = "provinces"
-FORMATS_LIST = ".txt"
+from tests import *
 
-comp100 = -0.083
-comp95 = -0.079
-comp70 = -0.058
 
-print(f"{comp70/70}:{comp70}/{-0.00083*70}",
-      f"{comp95/95}:{comp95}/{-0.00083*95}",
-      f"{comp100/100}:{comp100}/{-0.00083*100}")
+# Печатать ли результаты
+print_text = True
+
+for test in [Core, NonCore]:
+    x = test()
+    for region in x.country.non_core_regions:
+        print(x.country.name, region.name, region.global_id)
+
