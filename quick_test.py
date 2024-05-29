ae80f5f675b12c4f490c98df1c539479ee79859d
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-29 16:05:01 +0300

    Update quick_test.py

--- quick_test.py
+++ quick_test.py
@@ -1,10 +1,19 @@
 from read_presets.read_preset import read_preset
-from read_presets.preset_into_country import turn_preset_into_country
+from constants_and_settings.constants import (
+    PRESETS_PATH, COMMON_TRADE_PATH, COMMON_TECH_PATH
+)
+from read_presets.preset_into_country import (
+    turn_preset_into_country, add_events_to_country
+)
 
-
-result = read_preset("sov")
-country = turn_preset_into_country(result)
+country_preset = read_preset("sov", PRESETS_PATH)
+tech_preset = read_preset("casual", COMMON_TECH_PATH)
+trade_preset = read_preset("sov", COMMON_TRADE_PATH)
+country = turn_preset_into_country(country_preset)
+add_events_to_country(country, trade_preset)
+# add_events_to_country(country, tech_preset)
+a = 0
 for event in country.events:
-    print(event)
-# for region in country.regions:
-#     print(region.name)
+    a += 1
+
+print(a)
