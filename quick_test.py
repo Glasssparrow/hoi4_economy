4eb8da36d9b5011cf1a439fdcea28ed837ea4f7c
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-29 15:22:40 +0300

    Update quick_test.py

--- quick_test.py
+++ quick_test.py
@@ -1,7 +1,8 @@
 from read_presets.read_preset import read_preset
+from read_presets.preset_into_country import turn_preset_into_country
 
 
 result = read_preset("sov")
-print(result)
-for region in result.regions:
+country = turn_preset_into_country(result)
+for region in country.regions:
     print(region.name)
