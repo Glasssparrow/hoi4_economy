7515f1271c02fbd35e73235b3ec3c61547e7d893
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-29 15:21:38 +0300

    refactoring

--- read_presets/read_preset.py
+++ read_presets/read_preset.py
@@ -1,6 +1,4 @@
 from os import path, listdir
-from simulation_code.create_a_country import get_data, get_country
-from simulation_code.events import Event
 from constants_and_settings.constants import PRESETS_FOLDER
 
 
@@ -30,20 +28,3 @@ def read_preset(preset_name):
         if edited_line:  # Все не пустые строки в приказы
             orders.append(edited_line)
     return orders
-
-
-def turn_preset_into_country(orders):
-    # Преобразуем лист приказов в экземпляр Country
-    with open("tags.txt", "r") as json_file:
-        all_tags = json_file.read()  # Список кодов стран
-    data = get_data()                # Данные по регионам
-    if orders[0].upper() not in all_tags:
-        by_tag = False
-    else:
-        by_tag = True
-    # Формирование экземпляра.
-    name_or_tag = orders.pop(0)
-    country = get_country(data=data, name_or_tag=name_or_tag, by_tag=by_tag)
-    for order in orders:
-        country.add_event(Event(order))
-    return country
