0940128f172c52de273b0a21de66c886b180bf96
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-29 22:21:16 +0300

    Update construct_civil_and_mil.py

--- automated_construction/construct_civil_and_mil.py
+++ automated_construction/construct_civil_and_mil.py
@@ -28,10 +28,10 @@ def _add_queue(country, switch_point, counter=None):
 
 
 def auto_construct_without_infrastructure(
-        country, simulation_length, cycle_length, switch_point=0
+        country, simulation_length, cycle_length, switch_point=5000
 ):
     counter = _add_queue(country, switch_point,)
-    for day in simulation_length:
+    for day in range(simulation_length):
         country.calculate_day(day)
         if day % cycle_length == 0:
             counter = _add_queue(country, switch_point, counter=counter)
