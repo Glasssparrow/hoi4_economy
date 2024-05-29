1a5d6783119c009e7e1d6e99f26cb9abdc36eea6
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-29 18:02:04 +0300

    Update construct_civil_and_mil.py

--- automated_construction/construct_civil_and_mil.py
+++ automated_construction/construct_civil_and_mil.py
@@ -1,6 +1,14 @@
 
 
-def _add_queue(country, switch_point, counter=0):
+class Counter:
+
+    def __init__(self):
+        self.number = 0
+
+
+def _add_queue(country, switch_point, counter=None):
+    if not counter:
+        counter = Counter()
     regions = country.regions
     queue = country.queue
     return counter
