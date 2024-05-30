477f52ae2ddb9d02b90cbfd1cd4958aa3bd89d94
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-30 20:14:26 +0300

    autoconstruct
    not ready. Can't construct in regions with different lvl of infrastructure

--- automated_construction/construct_civil_and_mil.py
+++ automated_construction/construct_civil_and_mil.py
@@ -1,9 +1,14 @@
+from constants_and_settings.constants import (
+    CIVIL_BUILDING, MILITARY_BUILDING
+)
+from simulation_code.queue import Order
 
 
 class Counter:
 
     def __init__(self):
-        self.number = 0
+        self.factories = 0
+        self.mil_factories = 0
 
 
 def _get_regions_with_good_infrastructure(regions):
@@ -24,19 +29,70 @@ def _get_regions_with_good_infrastructure(regions):
     return good_inf_regions
 
 
-def _add_queue(country, switch_point, counter=None):
+def _add_queue(country, switch_point, counter=None,
+               queue_length=2, start_point=False,):
     if not counter:
         counter = Counter()
     regions = country.regions
     queue = country.queue
+    if not start_point:
+        if len(queue) < country.factories/15:
+            raise Exception("Строительство простаивает!")
     good_inf_regions = _get_regions_with_good_infrastructure(regions)
+    for region in good_inf_regions:
+        if len(queue) < (country.factories/15+queue_length):
+            if (
+                counter.factories > switch_point
+            ):
+                counter.mil_factories += region.available_for_queue
+                country.add_order(Order(
+                    region_name=region.name,
+                    building_type=CIVIL_BUILDING,
+                    quantity=region.available_for_queue,
+                ))
+            elif (
+                counter.factories+region.available_for_queue >
+                switch_point
+            ):
+                mil_build = (
+                    counter.factories + region.available_for_queue -
+                    switch_point
+                )
+                civil_build = region.available_for_queue - mil_build
+                counter.factories += civil_build
+                counter.mil_factories += mil_build
+                country.add_order(Order(
+                    region_name=region.name,
+                    building_type=CIVIL_BUILDING,
+                    quantity=civil_build,
+                ))
+                country.add_order(Order(
+                    region_name=region.name,
+                    building_type=MILITARY_BUILDING,
+                    quantity=mil_build,
+                ))
+            elif (
+                counter.factories + region.available_for_queue < switch_point
+            ):
+                counter.factories += region.available_for_queue
+                country.add_order(Order(
+                    region_name=region.name,
+                    building_type=MILITARY_BUILDING,
+                    quantity=region.available_for_queue,
+                ))
+        else:
+            break
+        # print(
+        #     region.name, region.infrastructure, region.available_for_queue,
+        #     region.global_id,
+        # )
     return counter
 
 
 def auto_construct_without_infrastructure(
-        country, simulation_length, cycle_length, switch_point=5000
+        country, simulation_length, cycle_length, switch_point=999
 ):
-    counter = _add_queue(country, switch_point,)
+    counter = _add_queue(country, switch_point, start_point=True)
     for day in range(simulation_length):
         country.calculate_day(day)
         if day % cycle_length == 0:
