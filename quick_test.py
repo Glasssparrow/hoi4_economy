80ce4dfd1bcf1269be31e4e11a4d0bcb06c0c42e
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-30 23:31:42 +0300

    Update quick_test.py

--- quick_test.py
+++ quick_test.py
@@ -7,13 +7,30 @@ from read_presets.preset_into_country import (
 )
 from automated_construction.construct_civil_and_mil import (
     auto_construct_without_infrastructure)
+from simulation_code.region import Region
+from constants_and_settings.constants import CIVIL_BUILDING
 
-country_preset = read_preset("sov", PRESETS_PATH)
-tech_preset = read_preset("casual", COMMON_TECH_PATH)
-trade_preset = read_preset("sov", COMMON_TRADE_PATH)
-country = turn_preset_into_country(country_preset)
-add_events_to_country(country, trade_preset)
-add_events_to_country(country, tech_preset)
-auto_construct_without_infrastructure(
-    country, 480, 50, 7,
+# country_preset = read_preset("sov", PRESETS_PATH)
+# tech_preset = read_preset("casual", COMMON_TECH_PATH)
+# trade_preset = read_preset("sov", COMMON_TRADE_PATH)
+# country = turn_preset_into_country(country_preset)
+# add_events_to_country(country, trade_preset)
+# add_events_to_country(country, tech_preset)
+
+# country_preset = read_preset("sik", PRESETS_PATH)
+# country = turn_preset_into_country(country_preset)
+#
+# auto_construct_without_infrastructure(
+#     country, 480, 50, 7,
+# )
+
+region = Region(
+    "Dzungaria",
+    2,
+    ["sik"],
+    1,
+    2,
+    0,
+    0
 )
+region.add_queue(2, CIVIL_BUILDING)
