2f5de54adf0b19ce7856a078124333c86adcda4b
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-30 15:22:41 +0300

    Update quick_test.py

--- quick_test.py
+++ quick_test.py
@@ -15,5 +15,5 @@ country = turn_preset_into_country(country_preset)
 add_events_to_country(country, trade_preset)
 add_events_to_country(country, tech_preset)
 auto_construct_without_infrastructure(
-    country, 500, 250,
+    country, 480, 250,
 )
