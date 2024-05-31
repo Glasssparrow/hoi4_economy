cfaf098dfe4c3cda7421ff3a9b57fbb344917763
Glass <125502088+Glasssparrow@users.noreply.github.com>
2024-05-31 20:39:24 +0300

    Update region.py

--- simulation_code/region.py
+++ simulation_code/region.py
@@ -88,9 +88,9 @@ class Region:
         )
 
     def add_queue(self, quantity, building_type):
-        if quantity == 0:
+        if quantity <= 0:
             raise Exception(
-                f"Добавление приказа на строительство 0 зданий "
+                f"Добавление приказа на строительство <= 0 зданий "
                 f"в регионе {self.name}"
             )
         if building_type in [MILITARY_BUILDING, CIVIL_BUILDING]:
